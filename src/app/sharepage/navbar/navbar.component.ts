3201eef8cb80d4bb01cb455651f70d4bde5c7db4
Ursu44 <cosminaursulesei23@gmail.com>
2023-10-21 08:32:27 +0300

    Commit 4

--- src/app/sharepage/navbar/navbar.component.ts
+++ src/app/sharepage/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    if (window.pageYOffset > 100) {
+      this.isScrolled = true;
+    } else {
+      this.isScrolled = false;
+    }
+  }
 }
