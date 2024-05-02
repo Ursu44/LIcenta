1e98bd6c74e1944f97e42e3323adeca0f87c0f17
Ursu44 <cosminaursulesei23@gmail.com>
2024-05-02 22:00:28 +0300

    commit nou 13

--- src/main/java/app/micronaut/Controler.java
+++ src/main/java/app/micronaut/Controler.java
@@ -1,8 +1,12 @@
 package app.micronaut;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.annotation.Put;
@@ -13,7 +17,7 @@ import jakarta.inject.Inject;
 import java.security.Principal;
 
 @Secured(SecurityRule.IS_AUTHENTICATED)
-@Controller
+@Controller("/update")
 public class Controler {
     @Inject
     private DBInitializer dbInitializer;
@@ -29,10 +33,31 @@ public class Controler {
     }
 
     @Produces(MediaType.TEXT_PLAIN)
-    @Put
+    @Put("/progres")
     public void index(Principal principal) {
         String response = principal.getName();
         String gmail = response.split("_")[1];
         updateCurs.update(gmail);
+    }
+
+    @Produces(MediaType.TEXT_PLAIN)
+    @Put("/timp")
+    public void index1(Principal principal,  @Body String json) throws JsonProcessingException {
+        String response = principal.getName();
+        String gmail = response.split("_")[1];
+        //System.out.println("Mail "+gmail+" "+json);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode raspunsJson = objectMapper.readTree(json);
+        String an = raspunsJson.get("materie").asText().split("_")[1];
+        String materie = raspunsJson.get("materie").asText().split("_")[0];
+        String ora =  raspunsJson.get("ora").asText();
+        int incercari =  raspunsJson.get("incercari").asInt();
+
+        /*System.out.println("An "+an);
+        System.out.println("Materie "+materie);
+        System.out.println("Ora "+ora);
+        System.out.println("Incercari "+incercari);*/
+
+        updateCurs.updateTest(gmail, an, materie, ora, incercari);
     }
 }
--- src/main/java/app/micronaut/UpdateCurs.java
+++ src/main/java/app/micronaut/UpdateCurs.java
@@ -4,6 +4,7 @@ import com.google.firebase.database.*;
 import jakarta.inject.Inject;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -37,6 +38,41 @@ public class UpdateCurs {
                             HashMap<String, Object>  data = new HashMap<>();
                             data.put("progres", progress);
                             lectieSnapshot.getRef().updateChildren(data, null);
+                    }
+                }
+                latch.countDown();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                System.out.println("Eroare la extragerea cursurilor: " + error.getMessage());
+                latch.countDown();
+            }
+        });
+
+        try {
+            latch.await(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void updateTest(String mail, String an, String materie, String ora,int incercari){
+        CountDownLatch latch = new CountDownLatch(1);
+        materii.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot studentSnapshot : dataSnapshot.getChildren()) {
+                    //System.out.println("Mail de cautat "+studentSnapshot.child(materie).child("mailProfesor"));
+                    String extractedMail = studentSnapshot.child(materie).child("mailProfesor").getValue(String.class);
+                    String extractedAn = studentSnapshot.child("an").getValue(String.class);
+                    if (!extractedMail.equals("") && extractedMail.equals(mail) && extractedAn.equals(an)) {
+                        extractedMail = extractedMail.replace(',', '.');
+                        System.out.println("Am intrat" + extractedMail);
+                        Map<String, Object> data = new HashMap<>();
+                        data.put("durataTest", ora);
+                        data.put("incercari", incercari);
+                        studentSnapshot.child(materie).getRef().updateChildren(data, null);
                     }
                 }
                 latch.countDown();
